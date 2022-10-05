3170aa21b7b5873244aa08467176ca4d5ab17307
facuzarate04 <82891956+facuzarate04@users.noreply.github.com>
2022-10-05 18:57:26 -0300

    add validation file

--- src/notification/email/validation.ts
+++ src/notification/email/validation.ts
@@ -1,15 +1,63 @@
-/* Validate */
+import { ICreatedOrderRequest } from "./created_order";
+import { IRejectedOrderRequest } from "./rejected_order";
 
-export function requiredField (field: any) {
-    if (!field) {
-        return {path: field, message: `${field} can not be empty`};
-    }
-    return;
+
+interface ValidationError {
+    code?: string;
+    messages: ValidationErrorItem[];
+}
+interface ValidationErrorItem {
+    field: string;
+    message: string;
 }
 
-export function stringField (field: any) {
-    if (typeof field !== 'string') {
-        return {path: field, message: `${field} should be an string`};
+
+export function validateSendCreatedOrder(body: ICreatedOrderRequest): Promise<ICreatedOrderRequest> {
+    const result: ValidationError  = {
+        messages: []
+    };
+
+    if (!body.name) {
+        result.messages.push({ field: 'name', message: 'Name is required' });
     }
-    return;
+    if (!body.order_number) {
+        result.messages.push({ field: 'order_number', message: 'Order number is required' });
+    }
+    if (!body.from) {
+        result.messages.push({ field: 'from', message: 'From is required' });   
+    }
+    if (!body.to) {
+        result.messages.push({ field: 'to', message: 'To is required' });
+    }
+    if (result.messages.length > 0) {
+        return Promise.reject(result);
+    }
+
+    return Promise.resolve(body);
+
+}
+
+export function validateSendRejectedOrder(body: IRejectedOrderRequest): Promise<IRejectedOrderRequest> {
+    const result: ValidationError  = {
+        messages: []
+    };
+
+    if (!body.name) {
+        result.messages.push({ field: 'name', message: 'Name is required' });
+    }
+    if (!body.reason) {
+        result.messages.push({ field: 'reason', message: 'Reason number is required' });
+    }
+    if (!body.from) {
+        result.messages.push({ field: 'from', message: 'From is required' });   
+    }
+    if (!body.to) {
+        result.messages.push({ field: 'to', message: 'To is required' });
+    }
+    if (result.messages.length > 0) {
+        return Promise.reject(result);
+    }
+
+    return Promise.resolve(body);
+
 }
