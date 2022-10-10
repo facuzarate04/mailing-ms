9c8bd543f786ee8a654ded82592ff0addd88951e
facuzarate04 <82891956+facuzarate04@users.noreply.github.com>
2022-10-10 16:06:48 -0300

    fixed untracked files

--- dist/notifications/templates/created_order.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.body = void 0;
-const body = (title, body) => {
-    return `<h2>${title}</h2> <p>${body}</p>`;
-};
-exports.body = body;
